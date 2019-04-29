0ea69f8eb7ceceeee8d3f950f1e6b64b61f09f2d
vinigandara <50087804+vinigandara@users.noreply.github.com>
2019-04-28 21:34:42 -0300

    Update manage.py

--- mysite/manage.py
+++ mysite/manage.py
@@ -14,8 +14,7 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) 
-        from exc
-    execute_from_command_line(sys.argv)
+        from exc import execute_from_command_line(sys.argv)
 
 
 if __name__ == '__main__':
